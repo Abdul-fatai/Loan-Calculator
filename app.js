e184ec960e226a5f811b60e2b12ed821afe566d1
Abdul-fatai <staticdev20046@gmail.com>
2019-10-14 12:03:03 +0100

    loader added

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // Listen for Submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    e.preventDefault();
+    // Hide results 
+    document.getElementById('results').style.display = 'none';
+
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+});
 
 
 // Calculate result
-function calculateResults(e){
-    e.preventDefault();
+function calculateResults(){
    
     //UI Vars
     const amount = document.getElementById('amount');
@@ -26,6 +34,13 @@ function calculateResults(e){
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        //Show Results
+        document.getElementById('results').style.display = 'block';
+
+        //Hide Loader
+        document.getElementById('loading').style.display = 'none';
+
     } else{
         showError("Please check your numbers");
     }
@@ -35,6 +50,12 @@ function calculateResults(e){
 
 function showError(error){
 
+    //Show Results
+    document.getElementById('results').style.display = 'none';
+
+    //Hide Loader
+    document.getElementById('loading').style.display = 'none';
+    
     //Create Element 
     const errorDiv = document.createElement('div');
 
